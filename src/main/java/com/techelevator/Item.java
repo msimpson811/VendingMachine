c6db398455ee9e10f4b81a13b72ddf36948975d7
Allie Ortlieb <aortlieb2358@gmail.com>
2021-02-06 17:56:38 -0500

    the good stuff

--- src/main/java/com/techelevator/Item.java
+++ src/main/java/com/techelevator/Item.java
@@ -1,18 +1,34 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 public class Item {
 	private String name;
 	private BigDecimal price;
+	private int quantity = 5;
 	
+	public int decreaseQuantity() {
+		quantity = quantity -1;
+		return quantity;
+	}
 	
+	public String getQuantity() {
+		if (quantity == 0) {
+			return "Sorry Item is sold out.";
+		}
+	
+		return String.valueOf(quantity);
+	}
+
+
+
 	public Item(String name, BigDecimal price) {
 		this.name = name;
 		this.price = price;
 	}
 	
-
+	
 
 	public String getName() {
 		return name;
---
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -0,0 +1,17 @@
+package com.techelevator;
+
+public class VendingMachine {
+	private ItemInventory items;
+
+	public ItemInventory getItems() {
+		return items;
+	}
+
+	public VendingMachine() {
+		this.items = new ItemInventory();
+	}
+	
+	
+	
+
+}
