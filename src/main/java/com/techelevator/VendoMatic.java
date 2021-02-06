addc67800cbd5eba8f1e56bfbdc6c56e7a47bb66
Mark Simpson <msimpson811@gmail.com>
2021-02-06 17:41:59 -0500

    VendingMachineUser

---
+++ src/main/java/com/techelevator/VendoMatic.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+
+public class VendoMatic {
+
+	public static void main(String[] args) {
+	
+		
+	    
+       VendingMachineUser vending = new VendingMachineUser();
+       vending.startMenu();
+        
+       
+	}
+}
